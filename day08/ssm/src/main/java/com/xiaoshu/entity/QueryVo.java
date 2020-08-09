16726e2b1332510c4885490fd542a3d57110615c
yangxiaopeng <381531980@qq.com>
2020-08-09 22:49:50 +0800

    day08

---
+++ day08/ssm/src/main/java/com/xiaoshu/entity/QueryVo.java
@@ -0,0 +1,14 @@
+package com.xiaoshu.entity;
+
+public class QueryVo extends Student {
+	 private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	 
+}
